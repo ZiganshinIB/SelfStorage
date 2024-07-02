f97f70eaf98624d971b4c1c228a4ab24bc354c2a
malykhdenis <85461091+malykhdenis@users.noreply.github.com>
2024-07-02 22:25:17 +0300

    Update and rename 0020_rent_partial.py to 0021_rent_partial.py

--- selfstorage/storage/migrations/0021_rent_partial.py
+++ selfstorage/storage/migrations/0021_rent_partial.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('storage', '0019_rent_delivery'),
+        ('storage', '0020_rent_delivery'),
     ]
 
     operations = [
